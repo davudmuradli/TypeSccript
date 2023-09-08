9c5430f66b9a0ad43890f0fbfa7a34abb450ae80
damur <133804991+davudmuradli@users.noreply.github.com>
2023-09-08 20:35:43 +0300

    Update index.ts

--- TypeMart/index.ts
+++ TypeMart/index.ts
@@ -1,7 +1,11 @@
-import products from './products';
+import products from "./products";
 
-const productName : string ='beanie';
-
+let productName: string = "fanny pack";
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+const shippingAdress: string = "575 Broadway, New York City, New York";
 
 const filteredProducts = products.filter((product) => {
   return productName === product.name;
@@ -9,11 +13,30 @@ const filteredProducts = products.filter((product) => {
 
 if (filteredProducts.length > 0) {
   const product = filteredProducts[0];
-  if (product.preOrder === true) { // Assuming preOrder is a string 'true' or 'false'
+  if (product.preOrder === true) {
+    // Assuming preOrder is a string 'true' or 'false'
     console.log("We will send you a message when it is on the way!");
+  } else {
+    console.log(`Product '${product.name}' not found.`);
   }
-  else {
-  console.log(`Product '${product.name}' not found.`);
+}
+
+  if (filteredProducts[0].price > 25) {
+     shipping = 0;
+    console.log("Your Shipping is free.");
+  } else {
+     shipping = 5;
+    console.log(`Shipping is '${shipping}' euros.`);
   }
-  console.log(product);
-  } 
+
+if(shippingAdress.match('New York')){
+   taxPercent = 0.1;  
+}else{
+   taxPercent = 0.5;
+}
+
+taxTotal = filteredProducts[0].price * taxPercent;
+total = filteredProducts[0].price + taxTotal + shipping;
+
+
+console.log(`Receipt: \n Product Name: ${filteredProducts[0].name} \n Shipping Adress: ${shippingAdress} \n Price of the product: ${filteredProducts[0].price} USD \n Tax Total: ${taxTotal} USD \n Shipping: ${shipping} USD \n \n Total Amount: ${total} USD`)
