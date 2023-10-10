3e7f2a3c1f46d59835956dd5b552b0ed5bcdcdf4
damur <133804991+davudmuradli@users.noreply.github.com>
2023-10-10 17:14:40 +0300

    Create unions.ts

---
+++ unions.ts
@@ -0,0 +1,13 @@
+type Status = 'idle' | 'downloading' | 'complete';
+
+function downloadStatus(status: Status){
+  if( status === 'idle'){
+    console.log('Download')
+  }else if(status === 'downloading'){
+    console.log('Downloading')
+  }else if(status === 'complete'){
+    console.log('Your download is complete!')
+  }
+};
+
+downloadStatus('idle');
